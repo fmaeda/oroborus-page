e702ca24c135b4f561d748eb64bb9557a2689793
Fabiano Maeda <fabianomaeda@gmail.com>
2021-08-29 20:09:49 -0300

    - pagina temporaria

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, createRef } from 'react';
 import './App.css';
 
-function App() {
+import logo from 'images/oroborus-light.png';
+
+const App = () => {
+  const animationContainer = createRef<HTMLDivElement>();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} />
+      <p>Em breve</p>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Teste.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type Props = {};
+
+class Teste extends React.Component<Props> {
+  render() {
+    return <div>Teste</div>;
+  }
+}
+
+export default Teste;
